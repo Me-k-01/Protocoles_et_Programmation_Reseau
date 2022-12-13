fe30f1de7cddb5b0d1f04eec2da0d8c0ebab6146
Me-k-01 <mecaman01@gmail.com>
2022-12-14 00:03:18 +0100

    correction des port

--- proxy.py
+++ proxy.py
@@ -1,22 +1,32 @@
 #!/usr/bin/python3
 import socket, re
 
-IP_PROXY = '0.0.0.0' 
-PORT_PROXY = 80
+IP_PROXY = 'localhost' 
+PORT_PROXY = 8000
 
 ############### Fonctions ###############
 def rcvall(socket) :
     rep = []
 
     while True :
-        reponse = socket.recv(1000)
+        # socket.recv(1000) est une sortie bloquante, donc ce n'est pas une bonne solutions
+        reponse = socket.recv(1000)  # TODO: trouver un moyen d'extraire l'entiereté des données d'une page web
         if not reponse : break
-        
         rep.append(reponse)
-    
-    reponse = b''.join(rep)
-    return reponse
+        
+    return  b''.join(rep)
 
+
+def formatRequest(request):
+    lignes = request.decode('utf-8').split('\r\n') 
+    res = []
+    for line in lignes:
+        if line.startswith("Connection: keep-alive"): 
+            continue
+        if line.startswith("Proxy-Connection: keep-alive") :
+            continue
+        res.append(line)
+    return '\r\n'.join(res).encode('utf-8') 
 
 ############### Set up et démarage du proxy ###############
 ma_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
@@ -25,41 +35,39 @@ ma_socket.bind((IP_PROXY, PORT_PROXY))
 ma_socket.listen(socket.SOMAXCONN)
 print("Lancement du proxy:", IP_PROXY, ":", PORT_PROXY)
 
-# Attente d'une nouvelle connexion
 while True:
-    socket_client, addr = ma_socket.accept()
+    # Attente d'une nouvelle connexion.
+    socket_client, addr = ma_socket.accept() # renvoie le socket du client vers le proxy
     print ("Nouvelle connexion depuis: ", addr)
-    
-    # request = client_co.recv(1000).decode()
-    request = socket_client.recv(1000).decode('utf-8')
-    
-    # Isolement de chaque partie de la reponse séparé par \r\n pour retirer les lignes commençant par Connection:keep-alive et Proxy-connection:keep_alive    
-    elements = request.split('\r\n')
-    elements.remove(elements[3]) #elements.remove("Connection: Keep-Alive") 
-    elements.remove(elements[2]) #elements.remove("Proxy-Connection: Keep-Alive")  
 
-    # On extrait l'adresse du serveur pour se connecter dessus
-    host = re.search('(?<=: )[^\]]+', elements[2])
+    request = socket_client.recv(1000)
+    print(request)
+    # Isolement de chaque partie de la reponse séparé par \r\n ,
+    # pour retirer les lignes commençant par Connection:keep-alive et Proxy-connection:keep_alive .  
+    #elements.remove(elements[3]) 
+    #elements.remove(elements[2])  
+    # On extrait l'adresse du serveur pour se connecter dessus.
+    #host = re.search('(?<=: )[^\]]+', request)
     # On recompose le message à envoyer au serveur
-    msg_to_send = '\r\n'.join(elements).encode('utf-8') 
+    msg_to_send = formatRequest(request)
 
+    print("Requête reçu: ", request.decode('utf-8'))  
+    print("Requête à faire: ", msg_to_send.decode('utf-8')) 
+    #print("Addresse du serveur à joindre:", host[0])
     
-    
-    print("Requête reçu: ", request)  
-    print("Requête à faire: ", elements) 
-    #print(host[0])
-    
+    # Socket du proxy vers le serveur
     socket_proxy = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
     socket_proxy.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     
     #TODO: ces valeurs sont là pour les test à rendre dynamique plus tard
-    socket_proxy.connect(('p-fb.net', 443))
+    socket_proxy.connect(('p-fb.net', 80)) # pour du https on utilise port: 443
     
     socket_proxy.sendall(msg_to_send)
-    reponse = rcvall(socket_proxy)
+    reponse = rcvall(socket_proxy) #.recv(1000)
     
-    #print("Réponse du serveur: ", reponse.decode())
+    print("Réponse du serveur: ", reponse.decode('utf-8'))
     socket_client.sendall(reponse)
+    # Fin de la connection
     socket_client.close()
     
 
