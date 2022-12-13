e8e9dcc48c4291d9915685f886dc2f0344582c74
Me-k-01 <mecaman01@gmail.com>
2022-12-14 00:42:10 +0100

    HTTP et TLS qui fonctionne

--- proxy.py
+++ proxy.py
@@ -18,15 +18,34 @@ def rcvall(socket) :
 
 
 def formatRequest(request):
-    lignes = request.decode('utf-8').split('\r\n') 
+    # Isolement de chaque partie de la reponse séparé par \r\n ,
+    lignes = request.split('\r\n') 
     res = []
     for line in lignes:
+        # pour retirer les lignes commençant par Connection:keep-alive et Proxy-connection:keep_alive .  
         if line.startswith("Connection: keep-alive"): 
             continue
         if line.startswith("Proxy-Connection: keep-alive") :
             continue
+        if line.startswith("Accept-Encoding: gzip") :
+            continue
         res.append(line)
-    return '\r\n'.join(res).encode('utf-8') 
+    return '\r\n'.join(res)
+
+def getHost(request):
+    #firstLine = request.partition('\n')[0]
+    #host = re.search('(?<=: )[^\]]+', firstLine) 
+    # trouver la ligne "Host: ip:port"
+    i = request.index("Host: ") + 5 
+    hostLine = request[i:]
+    hostLine = hostLine[:hostLine.index("\r\n")]
+
+    host = hostLine.split(':') 
+    #TODO: ces valeurs sont là pour les test à rendre dynamique plus tard
+    if request.startswith("GET"): # HTTP 
+        return (host[0].strip(), 80)
+    if request.startswith("CONNECT"): #TLS
+        return (host[0].strip(), int(host[1].strip())) #443
 
 ############### Set up et démarage du proxy ###############
 ma_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
@@ -40,31 +59,31 @@ while True:
     socket_client, addr = ma_socket.accept() # renvoie le socket du client vers le proxy
     print ("Nouvelle connexion depuis: ", addr)
 
-    request = socket_client.recv(1000)
-    print(request)
-    # Isolement de chaque partie de la reponse séparé par \r\n ,
-    # pour retirer les lignes commençant par Connection:keep-alive et Proxy-connection:keep_alive .  
-    #elements.remove(elements[3]) 
-    #elements.remove(elements[2])  
-    # On extrait l'adresse du serveur pour se connecter dessus.
-    #host = re.search('(?<=: )[^\]]+', request)
+    request = socket_client.recv(1000).decode('utf-8')  
     # On recompose le message à envoyer au serveur
     msg_to_send = formatRequest(request)
+    # On extrait l'adresse du serveur et le port pour se connecter dessus. 
 
-    print("Requête reçu: ", request.decode('utf-8'))  
-    print("Requête à faire: ", msg_to_send.decode('utf-8')) 
-    #print("Addresse du serveur à joindre:", host[0])
+    # TODO: le client tente parfois d'actualiser la page avec une requete vide, 
+    # il faut donc trouver un moyen de garder la destination.
+    # Pour le moment on ignore ce cas.
+    if (request == ""):
+        print("Requête vide")
+        socket_client.close()
+        continue
+    
+    print("Requête reçu: ", request)  
+    print("Requête à faire: ", msg_to_send)  
+    dest = getHost(msg_to_send) 
+    print("Addresse du serveur à joindre:", dest)
     
     # Socket du proxy vers le serveur
     socket_proxy = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
     socket_proxy.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    socket_proxy.connect(dest)  
     
-    #TODO: ces valeurs sont là pour les test à rendre dynamique plus tard
-    socket_proxy.connect(('p-fb.net', 80)) # pour du https on utilise port: 443
-    
-    socket_proxy.sendall(msg_to_send)
-    reponse = socket_proxy.recv(100000)
-    print(reponse)
+    socket_proxy.sendall(msg_to_send.encode('utf-8'))
+    reponse = socket_proxy.recv(1000000000) # Le nombre est tres grands pour des grosses pages comme p-fb.net
     #print("Réponse du serveur: ", reponse.decode('utf-8'))
     socket_client.sendall(reponse)
     # Fin de la connection
