438c8efb28290de20f0b0b4bf993f6b59e6701a2
Me-k-01 <mecaman01@gmail.com>
2022-12-20 15:07:12 +0100

    clean up

--- proxy.py
+++ proxy.py
@@ -24,31 +24,29 @@ def rcv_all(socket) :
     return res_data
 
 
-
-
 def from_url_to_chemin(request):
     lignes = request.split('\r\n') 
     G=re.compile(r"GET")
     P=re.compile(r"POST")
     
     msg_modifier=[]
-    res=""
+    res = ""
     for line in lignes :
         if G.search(line) :#or hpps.search(line) :
             
-            res=line[0:4]
-            i=13
+            res = line[0:4]
+            i = 13
             while True :
-                if(line[i] == "/") :
+                if line[i] == '/' :
                     break
-                i+=1
-            res+=line[i:]
+                i += 1
+            res += line[i:]
             msg_modifier.append(res)
 
         elif P.search(line):
 
-            res=line[0:5]
-            i=14
+            res = line[0:5]
+            i = 14
             while True :
                 if(line[i] == "/") :
                     break
@@ -62,30 +60,25 @@ def from_url_to_chemin(request):
 
 
   
-def filtre(request) :
-
-
+def filtre(request):
     doc=request.split(b"\r\n")
    
     html= doc[len(doc)-1].decode("utf-8",errors='ignore')
     try :
         fichier=open("./wordsBlackList.txt","r")
         
-      
         while True :
-
             ligne = fichier.readline()
 
             if not ligne:
                 break
             
             mot=re.compile(re.escape(ligne))
-
             html=re.sub(mot,"###",html)
     except Exception :
         print("erreur")
     
-    doc[len(doc)-1]=html.encode("utf-8")
+    doc[len(doc)-1] = html.encode("utf-8")
     reponse = b"\r\n".join(doc)
 
     return reponse
@@ -101,17 +94,12 @@ def cible_html (request):
         if G.search(line) or P.search(line):#or hpps.search(line) :
             i=0
             while 1 :
-                if(line[i]=="/" and line[i+1]!=" " ):
+                if line[i]=="/" and line[i+1] != " ":
                     return False
-                elif(line[i]=="/" and line[i+1]==" " ):
+                elif line[i]=="/" and line[i+1] == " ":
                     return True
-                i+=1
 
-        
-        
-
-           
-                
+                i += 1
 
 
 
@@ -146,13 +134,6 @@ def get_host(request):
     if request.startswith("POST"):  
         return host[0].strip(), 80
 
-def get_type(request):
-    if(request.startswith("GET")):
-        return "GET"
-    if(request.startswith("POST")):
-        return "POST"
-    if(request.startswith("CONNECT")):
-        return "CONNECT"
 
 def get_config_doc(): # Renvoie le document configurator.html
     #header = 'HTTP/1.1 200 OK\nContent-Type: text/html<strong>\n\n</strong>'
@@ -181,13 +162,7 @@ while True:
     # On recompose le message à envoyer au serveur
     msg_to_send = format_request(request)
     # On extrait l'adresse du serveur et le port pour se connecter dessus. 
-    
-    request_type = get_type(request)
-    #On recupère le type de la requête entre GET, POST et CONNECT pour pouvoir effectuer les traitements adéquats dessus
-    #print(request_type)
-    #if (request_type):
-    #    if(request_type[0] == 'GET'):
-    #        print("c'est du get")
+      
     ##### NE PAS RETIRER AVANT D'AVOIR RESOLUE LE PROBLEME SVP #####
     # TODO: le client tente parfois d'actualiser la page avec une requete vide, 
     # Faut-il l'envoyer quelque pars?
